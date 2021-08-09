ee3c0121f6edc630ba3ffa53623818684ce27ffe
Ricardo Lezama <lezama@lacartita.com>
2021-08-08 23:37:33 -0700

    Update org_per.py
    generate plot.

--- org_per.py
+++ org_per.py
@@ -25,7 +25,9 @@ def sacalasentidades(docs):
         for ent in nah.ents:
             if ent.label_ == 'GEO' or ent.label_ == 'PER':
                 list.append(ent.text) 
-    return list 
+    stopwords = open('corpora/stopwords.txt', 'r', encoding='utf-8').read().split("\n")
+    cancelled = [content for content in list if content not in stopwords]
+    return cancelled 
 
 def map_entities(tokens):
     """
@@ -58,17 +60,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def plot_terms(topic, data):
+def plot_terms_body(topic, data):
     """
-    Args: Topic is the name of the plot/category. 
+The no.aranage attribution is to understand how to best and programmatically 
+    plot the data. Intervals are determined by the counts within the dictionary. 
+    The user does not need to predefine the graph. Instead, the second line takes the 
+    frequency. Appropriate for body  metadata. 
     
-    Return frecuencia en encabezados.    
-    """
-    filter_ones = {term:frequency for term, frequency in data.items() if frequency != 1}  
-    terms = filter_ones.keys()
-    frequency = filter_ones.values()   
+    Args: Topic is the name of the plot/category. Data is a dictionary with frequency counts.
+    
+    No object returned. However, plot is automatically generated.
+        """
+    #The bar plot should be optimized for the max and min size of
+    #individual 
+    filter_ones = {term:frequency for term, frequency in data.items() if frequency > 10}  
+    filtered = {term:frequency for term, frequency in data.items() if frequency > round(sum(filter_ones.values())/len(filter_ones))  }   
+    print(round(sum(filtered.values())/len(filtered)), "Average count as result of total terms minus once identified terms divided by all terms.")
+    terms = filtered.keys()
+    frequency = filtered.values()   
     y_pos = np.arange(len(terms),step=1)
-    x_pos = np.arange(1,15, step=1)
+    # min dictionary value, max filtered value ; 
+    x_pos = np.arange(min(filtered.values()), max(filtered.values()), step=round(sum(filtered.values())/len(filtered)))
     plt.barh(y_pos, frequency, align='center', alpha=1)
     plt.yticks(y_pos, terms, fontsize=12)
     plt.xticks(x_pos)
@@ -77,3 +89,28 @@ def plot_terms(topic, data):
     plt.tight_layout()
     plt.show()
     
+def plot_terms_headline(topic, data):
+    """
+    The no.aranage attribution is to understand how to best and programmatically 
+    plot the data. Intervals are determined by the counts within the dictionary. 
+    The user does not need to predefine the graph. Instead, the second line takes the 
+    frequency
+    
+    Args: Topic is the name of the plot/category. Data is a dictionary with frequency counts.
+    
+    No object returned. However, plot is automatically generated.
+    """
+    filter_ones = {term:frequency for term, frequency in data.items() if frequency > 1}  
+    filtered = {term:frequency for term, frequency in filter_ones.items() if frequency > round(sum(filter_ones.values())/len(filter_ones))}  
+    print(round(sum(filtered.values())/len(filtered)), "Average count as result of total terms minus once identified terms divided by all terms.")
+    terms = filtered.keys()
+    frequency = filtered.values()   
+    y_pos = np.arange(len(terms),step=1)
+    x_pos = np.arange(min(filtered.values()), max(filtered.values()), step=round(sum(filtered.values())/len(filtered)))
+    plt.barh(y_pos, frequency, align='center', alpha=1)
+    plt.yticks(y_pos, terms, fontsize=12)
+    plt.xticks(x_pos)
+    plt.xlabel('Frecuencia en encabezados')
+    plt.title(str(topic), fontsize=14)
+    plt.tight_layout()
+    plt.show()
